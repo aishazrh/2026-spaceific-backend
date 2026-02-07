39a5e69774dc976cb0866267e38595bd5bebf688
Aisha Zarrah <aishazarrah@gmail.com>
2026-02-07 19:19:10 +0700

    chore(backend): init api project and add booking model & dbcontext

---
+++ Spaceific.Api/Spaceific.Api/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿namespace Spaceific.Api.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Room { get; set; } = "";
+        public DateTime Start {  get; set; }
+        public DateTime End { get; set; }
+        public bool AllDay { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Spaceific.Api/Spaceific.Api/Services/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Spaceific.Api.Models;
+
+namespace Spaceific.Api.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public required DbSet<Booking> Bookings { get; set; }
+
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
