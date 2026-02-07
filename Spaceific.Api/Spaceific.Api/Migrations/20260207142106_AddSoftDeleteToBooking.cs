2ebdd86f4140d8bf0d864128a6f76e2b5a1d0fd2
Aisha Zarrah <aishazarrah@gmail.com>
2026-02-07 21:26:00 +0700

    chore: update booking model and migration

---
+++ Spaceific.Api/Spaceific.Api/Migrations/20260207142106_AddSoftDeleteToBooking.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spaceific.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeleteToBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Bookings");
+        }
+    }
+}
--- Spaceific.Api/Spaceific.Api/Models/BookingDTO.cs
+++ Spaceific.Api/Spaceific.Api/Models/BookingDTO.cs
@@ -2,27 +2,23 @@
 
 namespace Spaceific.Api.Models
 {
-    public class Booking
+    public class BookingDTO
     {
-        public int Id { get; set; }
         [Required] 
         public string FirstName { get; set; } = string.Empty;
         [Required] 
         public string LastName { get; set; } = string.Empty;
         [Required] 
         public string Room { get; set; } = string.Empty;
-        [Required] 
-
+        [Required]
+        [MaxLength(500)]
         public string? Purpose { get; set; }
-        public DateTime Start {  get; set; }
+
+        public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public bool AllDay { get; set; }
 
         [Required] 
         public string Status { get; set; } = "Pending";
-        public bool IsDeleted { get; set; } = false;
-
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
