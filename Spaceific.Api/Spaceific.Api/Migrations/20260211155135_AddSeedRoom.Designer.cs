55960c7b6e1b7bc2341f5b3a61d01c16b5c3cc0f
Aisha Zarrah <aishazarrah@gmail.com>
2026-02-11 22:54:00 +0700

    chore: add seeder for entity 'room' + migrate

--- Spaceific.Api/Spaceific.Api/Migrations/20260211155135_AddSeedRoom.Designer.cs
+++ Spaceific.Api/Spaceific.Api/Migrations/20260211155135_AddSeedRoom.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spaceific.Api.Services;
 
@@ -11,9 +12,11 @@ using Spaceific.Api.Services;
 namespace Spaceific.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260211155135_AddSeedRoom")]
+    partial class AddSeedRoom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
